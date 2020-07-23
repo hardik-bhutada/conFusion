f5f4f06beda55adb6a4da470bf22db12e4cb540b
hardik-bhutada <hardik.bhutada@gmail.com>
2020-07-23 22:48:44 +0530

    basic services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,12 @@ import{MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component'
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import { DishService } from './services/dish.service';
 
 @NgModule({
   declarations: [
@@ -34,7 +35,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component'
     MatCardModule,
     MatButtonModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
