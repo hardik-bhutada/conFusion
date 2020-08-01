3aa1da141964c8fe3f1bc6324aeda277fbbcb37c
hardik-bhutada <hardik.bhutada@gmail.com>
2020-08-01 13:13:52 +0530

    Angular Routing Basics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
-const routes: Routes = [];
+
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
