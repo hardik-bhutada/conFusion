75c8eecb166e90adb3d83aa5e0d990e869fa0f73
hardik-bhutada <hardik.bhutada@gmail.com>
2020-08-04 17:16:30 +0530

    Single Page Applications Part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { ContactComponent } from './contact/contact.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'menu', component: MenuComponent },
+    { path: 'dishdetail/:id', component: DishdetailComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'contactus', component: ContactComponent }
 ];
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { DishService} from '../services/dish.service';
 
 
 @Component({
@@ -9,12 +12,22 @@ import { Dish } from '../shared/dish';
 })
 export class DishdetailComponent implements OnInit {
 
-  @Input()
+  // @Input()
   dish: Dish;
 
-  constructor() { }
+  id: number;
+
+  constructor(private dishService: DishService, 
+    private location: Location,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    let id = this.route.params['id'];
+    this.dish = this.dishService.getDish(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
